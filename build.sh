bcec3022bb0f0c31d46ba409fb1ca0bda39d5ca1
Bruce Becker <brucellino@gmail.com>
2015-01-18 22:20:11 +0100

    Added Build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+SOURCE_FILE=$NAME-$VERSION.tar.gz
+echo $SRC_DIR
+module load ci
+# SRC_DIR=/repo/src/$NAME
+#module load gcc/4.8.2
+echo "getting the file from gnu.org"
+if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
+  mkdir -vp $SRC_DIR
+#wget --verbose ftp://ftp.is.co.za/mirror/ftp.gnu.org/gnu/gsl/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
+  wget http://mirror.ufs.ac.za/gnu/gnu/$NAME/$NAME-$VERSION.tar.gz -O $SRC_DIR/$SOURCE_FILE
+fi
+ls -lht $SRC_DIR/$SOURCE_FILE
+tar xfz $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+cd $WORKSPACE/$NAME-$VERSION
+./configure --prefix $SOFT_DIR
+make -j 8
